38d9bbd92c7d847cbc582571f535ef7910ca24e4
Tarrant Reimers <tazreimers@hotmamail.com>
2022-05-17 14:02:50 +0800

    workshop on databases with tom

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,20 @@ import os
 from flask import Flask 
 from flask import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-from app import routes 
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dont-even-try-it-champ')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
